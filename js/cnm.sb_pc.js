1a838746ea01b411a5b2371adb66136200f1a632
杰尼龟 <96324819+JieNiGui06@users.noreply.github.com>
2022-01-18 16:54:17 +0800

    new

--- js/cnm.sb_pc.js
+++ js/cnm.sb_pc.js
@@ -17,7 +17,7 @@ window.onload=function()
 	let divTyping = document.getElementById('etext')
 	let i = 0,
 		timer = 0,
-		str = '千里之行，始于足下。'
+		str = '霆思相照\n		——JNG'
 	function typing()
 	{
 		if (i <= str.length)
