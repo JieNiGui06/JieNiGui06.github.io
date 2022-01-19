7d867f3e5ea25610407782c052f5ce38b24ab5dc
杰尼龟 <96324819+JieNiGui06@users.noreply.github.com>
2022-01-19 12:56:01 +0800

    new

--- js/cnm.sb_pc.js
+++ js/cnm.sb_pc.js
@@ -17,7 +17,7 @@ window.onload=function()
 	let divTyping = document.getElementById('etext')
 	let i = 0,
 		timer = 0,
-		str = '霆思相照\n		——JNG'
+		str = "霆思相照  "
 	function typing()
 	{
 		if (i <= str.length)
